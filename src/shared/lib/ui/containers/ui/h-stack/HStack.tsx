7cb6b0ebe7c1264e67c9b745633cd654cb4467aa
Дмитрий Бажаев <marmazy8@yandex.ru>
2024-01-30 15:29:13 +0300

    [FIX] Styles, imports

--- src/shared/lib/ui/containers/ui/h-stack/HStack.tsx
+++ src/shared/lib/ui/containers/ui/h-stack/HStack.tsx
@@ -3,5 +3,5 @@ import { Flex } from '../flex/Flex';
 import type { IHStackProps } from './HStack.interface';
 
 export const HStack: FC<IHStackProps> = (props) => {
-  return <Flex {...props} direction="row" />;
+  return <Flex {...props} justify="start" direction="row" />;
 };
--- src/shared/lib/ui/input/index.ts
+++ src/shared/lib/ui/input/index.ts
@@ -1,1 +1,3 @@
 export { Input } from './ui/input/Input';
+export { useInput } from './ui/input/Input.hooks';
+export type { IUseInputResult } from './ui/input/Input.interface';
